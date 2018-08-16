d8ec0db39e5d535c3ceebe9dc63235c86b7f1393
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-08-16 21:54:41 +0300

    Refactoring

--- app/src/main/java/com/kimlic/vendors/VendorsViewModel.kt
+++ app/src/main/java/com/kimlic/vendors/VendorsViewModel.kt
@@ -13,38 +13,24 @@ class VendorsViewModel : ViewModel(), LifecycleObserver {
     private val vendorsRepository = VendorsRepository.instance
     private val profileRepository = ProfileRepository.instance
 
-    //private var documentsLiveData = object : MutableLiveData<Vendors>() {}
     private var documentsForAdapter = object : MutableLiveData<List<Document>>() {}
-    //private var progressLiveData: SingleLiveEvent<Boolean> = object : SingleLiveEvent<Boolean>() {}
-    //private var responseObject: Vendors? = null
-
-    //private val vendorDocumentsLive = object : MutableLiveData<List<Document_>>() {}
 
     // public
 
-
     @OnLifecycleEvent(value = Lifecycle.Event.ON_START)
-    fun getDocumentslist() {
-        vendorsRepository.initDocuments(accountAddress = Prefs.currentAccountAddress)
-        Log.d("TAGVENDOR", "in repository on start")
-    }
+    fun getDocumentsList() = vendorsRepository.initDocuments(accountAddress = Prefs.currentAccountAddress)
 
-//    fun getVendorsDocuments(): LiveData<List<VendorDocument>> {
-//
-//        return documentsLiveData.value.documents
-//    }
 
     fun getDocumentsForAdapter() = documentsForAdapter// List Document which are supported in chosen country
 
     //fun progress() = progressLiveData
 
-    fun getSupportedDocuments_PostThemToAdaptersResult(country: String) {
+    fun supportedDocuments(country: String) {
         val userDocuments = profileRepository.documents(Prefs.currentAccountAddress)
         val linkedDocs = userDocuments.map { it.type to it }.toMap()
 
         // Documents are supported in chosen country
         val supportedDocuments = mutableListOf<Document>()
-
         val vendorsDocuments = vendorsRepository.vendorDocuments()
 
         vendorsDocuments.forEach { document ->
