7ce0924b50613e4d7574eb2bceb22f6c3e9ad57e
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-08-17 12:30:32 +0300

    New;

---
+++ app/src/main/java/com/kimlic/email/EmailViewModel.kt
@@ -0,0 +1,18 @@
+package com.kimlic.email
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import com.kimlic.model.ProfileRepository
+
+class EmailViewModel(application: Application) : AndroidViewModel(application) {
+
+    // Variables
+
+    private val repository = ProfileRepository.instance
+
+    // Public
+
+    fun emailVerify(email: String, onSuccess: () -> Unit, onError: () -> Unit) {
+        repository.verify("email", email, onSuccess, onError)
+    }
+}
