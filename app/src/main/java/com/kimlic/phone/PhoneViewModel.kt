61adbf8c44d86de5d0853445c5ca96b584632a88
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-08-17 14:46:58 +0300

    new;

--- app/src/main/java/com/kimlic/phone/PhoneViewModel.kt
+++ app/src/main/java/com/kimlic/phone/PhoneViewModel.kt
@@ -10,12 +10,16 @@ class PhoneViewModel(application: Application) : AndroidViewModel(application) {
     // Variables
 
     private val vendorRepository: VendorsRepository = VendorsRepository.instance
-    private val profileRepository: ProfileRepository = ProfileRepository.instance
+    private val repository: ProfileRepository = ProfileRepository.instance
 
     // Public
 
     fun verify(phone: String, onSuccess: () -> Unit, onError: () -> Unit) {
-        profileRepository.phoneVerify(phone, onSuccess, onError)
+        repository.contactVerify("phone", phone, onSuccess, onError)
+    }
+
+    fun emailApprove(code: String, onSuccess: () -> Unit, onError: (code: String) -> Unit) {
+        repository.contactApprove("phone", code, onSuccess, onError)
     }
 
     fun countries() = vendorRepository.countries()
