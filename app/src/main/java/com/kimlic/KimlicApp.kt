2367e6ec9265a3ee537a2708f0273db25e657946
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-08-18 10:35:16 +0300

    cleanup; refactoring;

--- app/src/main/java/com/kimlic/KimlicApp.kt
+++ app/src/main/java/com/kimlic/KimlicApp.kt
@@ -5,41 +5,31 @@ import android.content.Context
 import android.os.Build
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
-import android.annotation.SuppressLint
-import java.security.SecureRandom
-import javax.net.ssl.*
-import javax.security.cert.X509Certificate
-
 
 class KimlicApp : Application() {
 
-  // Constants
+    // Companion
 
-  private val TAG = this.javaClass.simpleName
+    companion object {
+        private var instance: KimlicApp? = null
 
-  // Companion
-
-  companion object {
-    private var instance: KimlicApp? = null
-
-    fun applicationContext(): Context =  instance!!.applicationContext
-  }
-
-  // Life
-
-  init {
-    instance = this
-  }
-
-  override fun onCreate() {
-    super.onCreate()
-    Fabric.with(this@KimlicApp, Crashlytics())
-  }
-
-  fun registerReceiver() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            val intentFilter = IntentFilter()
+        fun applicationContext(): Context = instance!!.applicationContext
     }
 
-  }
+    // Life
+
+    init {
+        instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Fabric.with(this@KimlicApp, Crashlytics())
+    }
+
+    fun registerReceiver() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val intentFilter = IntentFilter()
+        }
+    }
 }
