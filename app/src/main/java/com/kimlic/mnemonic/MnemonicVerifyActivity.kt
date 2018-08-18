7e3f3def95f2b1eaa63cd075b7e56f4d84f0cc87
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-08-18 19:25:43 +0300

    cleanup;

--- app/src/main/java/com/kimlic/mnemonic/MnemonicVerifyActivity.kt
+++ app/src/main/java/com/kimlic/mnemonic/MnemonicVerifyActivity.kt
@@ -73,7 +73,7 @@ class MnemonicVerifyActivity : BaseActivity() {
 
     private fun phrasesMatch(): Boolean {
         var noError = true
-        val mnemonicList = model.getUser(Prefs.currentAccountAddress).mnemonic.split(" ")
+        val mnemonicList = model.user(Prefs.currentAccountAddress).mnemonic.split(" ")
 
         for (i in 0 until hintList.size) noError = mnemonicList[hintList[i] - 1] == editTextList[i].text.toString()
 
