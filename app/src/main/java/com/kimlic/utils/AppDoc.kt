7b6f29191004fa42bc1e34c55c9df664526b6691
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-08-18 16:47:48 +0300

    new;

---
+++ app/src/main/java/com/kimlic/utils/AppDoc.kt
@@ -0,0 +1,8 @@
+package com.kimlic.utils
+
+enum class AppDoc(val type: String) {
+    PASSPORT("PASSPORT"),
+    ID_CARD("ID_CARD"),
+    DRIVERS_LICENSE("DRIVERS_LICENSE"),
+    RESIDENCE_PERMIT_CARD("RESIDENCE_PERMIT_CARD")
+}
