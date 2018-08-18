8f43f1581832488ea22c29b120b95908c48d4c55
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-08-18 19:20:23 +0300

    move retry function to viewModel

--- app/src/main/java/com/kimlic/MainActivity.kt
+++ app/src/main/java/com/kimlic/MainActivity.kt
@@ -3,12 +3,10 @@ package com.kimlic
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.os.Handler
 import com.kimlic.managers.PresentationManager
 import com.kimlic.model.ProfileViewModel
 import com.kimlic.preferences.Prefs
 import com.kimlic.splash.SplashScreenFragment
-import java.util.*
 
 class MainActivity : BaseActivity() {
 
@@ -16,7 +14,6 @@ class MainActivity : BaseActivity() {
 
     private lateinit var splashFragment: SplashScreenFragment
     private lateinit var model: ProfileViewModel
-    private val timeQueue = ArrayDeque<Long>(listOf(2000L, 4000L))
 
     // Life
 
@@ -34,7 +31,8 @@ class MainActivity : BaseActivity() {
         splashScreenShow()
 
         if (Prefs.authenticated) {
-            syncProfile(); quorumRequest()
+            syncProfile();
+            quorumRequest()
         } else signUpRecovery()
     }
 
@@ -51,7 +49,7 @@ class MainActivity : BaseActivity() {
     private fun quorumRequest() {
         model.quorumRequest(
                 onSuccess = { continueApp() },
-                onError = { retry() }
+                onError = { errorPopup("server error") }
         )
     }
 
@@ -72,12 +70,10 @@ class MainActivity : BaseActivity() {
     private fun signUpRecovery() {
         object : CountDownTimer(3000, 3000) {
             override fun onFinish() {
-                splashScreenHide(); PresentationManager.signupRecovery(this@MainActivity)
+                splashScreenHide(); PresentationManager.signUpRecovery(this@MainActivity)
             }
 
             override fun onTick(millisUntilFinished: Long) {}
         }.start()
     }
-
-    private fun retry() = timeQueue.poll()?.let { Handler().postDelayed({ quorumRequest() }, it) } ?: errorPopup("server error")
 }
