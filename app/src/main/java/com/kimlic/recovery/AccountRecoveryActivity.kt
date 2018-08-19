d9845ef57e03b22a3d99bb3d6bdba9f0be6933ef
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-08-19 15:33:06 +0300

    cleanup;

--- app/src/main/java/com/kimlic/recovery/AccountRecoveryActivity.kt
+++ app/src/main/java/com/kimlic/recovery/AccountRecoveryActivity.kt
@@ -1,11 +1,15 @@
 package com.kimlic.recovery
 
+import android.app.Activity
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.kimlic.BaseActivity
 import com.kimlic.R
 import com.kimlic.db.KimlicDB
+import com.kimlic.db.SyncService
 import com.kimlic.managers.PresentationManager
 import com.kimlic.preferences.Prefs
 import com.kimlic.quorum.QuorumKimlic
@@ -14,9 +18,14 @@ import kotlinx.android.synthetic.main.activity_account_recovery.*
 
 class AccountRecoveryActivity : BaseActivity() {
 
+    // Constants
+
+    private val GOOGLE_SIGNE_IN_REQUEST_CODE = 105
+
     // Variables
 
     private var recoveryViewModel: RecoveryViewModel? = null
+    //private lateinit var syncService: SyncService
 
     // Life
 
@@ -27,48 +36,78 @@ class AccountRecoveryActivity : BaseActivity() {
         setupUI()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            GOOGLE_SIGNE_IN_REQUEST_CODE -> {
+                if (resultCode != Activity.RESULT_OK) {
+                    Prefs.isUserGoogleSigned = false; finish()
+                }
+                Prefs.isUserGoogleSigned = true
+
+            }
+        }
+    }
+
     // Private
 
     private fun setupUI() {
         recoveryViewModel = ViewModelProviders.of(this).get(RecoveryViewModel::class.java)
 
-        backTv.setOnClickListener { PresentationManager.signupRecovery(this) }
-
+        backTv.setOnClickListener { PresentationManager.signUpRecovery(this) }
         verifyBt.setOnClickListener {
             val mnemonic = phraseEt.text.toString().trim()
 
             if (!mnemonicValid(mnemonic)) {
-                showPopup(title = "Error", message = "Missing mnemonic phrases"); return@setOnClickListener
+                showPopup(title = getString(R.string.error), message = getString(R.string.missing_mnemonic_phrases)); return@setOnClickListener
             }
 
             QuorumKimlic.destroyInstance()
             val quorumKimlic = QuorumKimlic.createInstance(mnemonic, this)
             val accountAddress = quorumKimlic.walletAddress
 
-            recoveryViewModel!!.retrievePhoto(accountAddress)
-
-            KimlicDB.getInstance()!!.close()
-            recoveryViewModel!!.retrieveDatabase(accountAddress = accountAddress, onSuccess = {
-                Prefs.authenticated = true
-                Prefs.currentAccountAddress = accountAddress
-                KimlicDB.getInstance()
-                Log.d(TAG, "Database restored successfully")
-                successfull()
-            }, onError = {
-                showPopup("Error", "Recovery Error")
-                return@retrieveDatabase
-            })
+            if (GoogleSignIn.getLastSignedInAccount(this) != null)
+                recoveryProfile(accountAddress)
+            else SyncService.signIn(this, GOOGLE_SIGNE_IN_REQUEST_CODE)
 
         }
     }
 
-    private fun getPhraseList(): List<String> {
-        val phraseText = phraseEt.text.toString()
-        val phraseList: List<String> = phraseText.trim().split(" ")
-        return phraseList
+    private fun recoveryProfile(accountAddress: String) {
+        KimlicDB.getInstance()!!.close()
+        recoveryViewModel!!.retrieveDatabase(accountAddress,
+                onSuccess = {
+
+                    Log.d(TAG, "Database restored successfully")
+                    recoveryViewModel!!.retrievePhoto(accountAddress,
+                            onSuccess = {
+                                Prefs.authenticated = true
+                                Prefs.currentAccountAddress = accountAddress
+                                KimlicDB.getInstance()
+                                Log.d(TAG, "Database restored successfully")
+                                successful()
+                            },
+                            onError = {
+                                errorPopup("Photos recovering Error!!!")
+                            })
+
+                    Prefs.authenticated = true
+                    Prefs.currentAccountAddress = accountAddress
+                    KimlicDB.getInstance()
+                    Log.d(TAG, "Database restored successfully")
+                    successful()
+                },
+                onError = {
+                    errorPopup("Recovery Error database not found!!!")
+                    return@retrieveDatabase
+                })
     }
 
-    private fun successfull() {
+    private fun getPhraseList(): List<String> {
+        val phraseText = phraseEt.text.toString()
+        return phraseText.trim().split(" ")
+    }
+
+    private fun successful() {
         val fragment = RecoverySuccesfulFragment.newInstance()
         fragment.setCallback(object : BaseCallback {
             override fun callback() {
