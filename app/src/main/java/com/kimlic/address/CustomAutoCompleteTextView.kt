c0a43845afa4537a1721ef4c94c052cafd6591f2
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-08-21 20:16:13 +0300

    AutoCompleteTextView with onBackPressed keyboard listener;

---
+++ app/src/main/java/com/kimlic/address/CustomAutoCompleteTextView.kt
@@ -0,0 +1,29 @@
+package com.kimlic.address
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.KeyEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.AutoCompleteTextView
+
+class CustomAutoCompleteTextView : AutoCompleteTextView {
+
+    // Constructor
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    constructor(context: Context) : super(context)
+
+    // Life
+
+    override fun onKeyPreIme(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            val mgr = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            mgr.hideSoftInputFromWindow(this.windowToken, 0)
+            // TODO: Hide your view as you do it in your activity
+        }
+        return false
+    }
+}
