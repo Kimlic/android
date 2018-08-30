f7350107e9078e4748364437b6536f32aa999092
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-08-30 13:10:50 +0300

    UserProfile recovering;

--- app/src/main/java/com/kimlic/recovery/AccountRecoveryActivity.kt
+++ app/src/main/java/com/kimlic/recovery/AccountRecoveryActivity.kt
@@ -4,15 +4,11 @@ import android.app.Activity
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.kimlic.BaseActivity
 import com.kimlic.R
-import com.kimlic.db.KimlicDB
 import com.kimlic.db.SyncService
 import com.kimlic.managers.PresentationManager
-import com.kimlic.preferences.Prefs
-import com.kimlic.quorum.QuorumKimlic
 import com.kimlic.utils.BaseCallback
 import kotlinx.android.synthetic.main.activity_account_recovery.*
 
@@ -25,7 +21,6 @@ class AccountRecoveryActivity : BaseActivity() {
     // Variables
 
     private var recoveryViewModel: RecoveryViewModel? = null
-    //private lateinit var syncService: SyncService
 
     // Life
 
@@ -33,92 +28,62 @@ class AccountRecoveryActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_recovery)
 
+        recoveryViewModel = ViewModelProviders.of(this).get(RecoveryViewModel::class.java)
         setupUI()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         when (requestCode) {
-            GOOGLE_SIGNE_IN_REQUEST_CODE -> {
-                if (resultCode != Activity.RESULT_OK) {
-                    Prefs.isUserGoogleSigned = false; finish()
-                }
-                Prefs.isUserGoogleSigned = true
-
-            }
+            GOOGLE_SIGNE_IN_REQUEST_CODE -> if (resultCode != Activity.RESULT_OK) finish()
         }
     }
 
     // Private
 
     private fun setupUI() {
-        recoveryViewModel = ViewModelProviders.of(this).get(RecoveryViewModel::class.java)
-
+        lifecycle.addObserver(recoveryViewModel!!)
+        SyncService.signIn(this, GOOGLE_SIGNE_IN_REQUEST_CODE)
         backTv.setOnClickListener { PresentationManager.signUpRecovery(this) }
-        verifyBt.setOnClickListener {
+
+        verifyBt.setOnClickListener { _ ->
             val mnemonic = phraseEt.text.toString().trim()
 
             if (!mnemonicValid(mnemonic)) {
                 showPopup(title = getString(R.string.error), message = getString(R.string.missing_mnemonic_phrases)); return@setOnClickListener
             }
 
-            QuorumKimlic.destroyInstance()
-            val quorumKimlic = QuorumKimlic.createInstance(mnemonic, this)
-            val accountAddress = quorumKimlic.walletAddress
-
-            if (GoogleSignIn.getLastSignedInAccount(this) != null)
-                recoveryProfile(accountAddress)
-            else SyncService.signIn(this, GOOGLE_SIGNE_IN_REQUEST_CODE)
-
+            GoogleSignIn.getLastSignedInAccount(this)?.let { recoveryUserProfile(mnemonic) }
         }
     }
 
-    private fun recoveryProfile(accountAddress: String) {
-        KimlicDB.getInstance()!!.close()
-        recoveryViewModel!!.retrieveDatabase(accountAddress,
-                onSuccess = {
-
-                    Log.d(TAG, "Database restored successfully")
-                    recoveryViewModel!!.retrievePhoto(accountAddress,
-                            onSuccess = {
-                                Prefs.authenticated = true
-                                Prefs.currentAccountAddress = accountAddress
-                                KimlicDB.getInstance()
-                                Log.d(TAG, "Database restored successfully")
-                                successful()
-                            },
-                            onError = {
-                                errorPopup("Photos recovering Error!!!")
-                            })
-
-                    Prefs.authenticated = true
-                    Prefs.currentAccountAddress = accountAddress
-                    KimlicDB.getInstance()
-                    Log.d(TAG, "Database restored successfully")
-                    successful()
-                },
-                onError = {
-                    errorPopup("Recovery Error database not found!!!")
-                    return@retrieveDatabase
+    private fun recoveryUserProfile(mnemonic: String) {
+        recoveryViewModel!!.recoveryProfile(mnemonic,
+                onSuccess = { successful() },
+                onError = { errorMessage ->
+                    errorPopup(errorMessage); return@recoveryProfile
                 })
     }
 
-    private fun getPhraseList(): List<String> {
-        val phraseText = phraseEt.text.toString()
-        return phraseText.trim().split(" ")
-    }
+    // Private helpers
 
     private fun successful() {
-        val fragment = RecoverySuccesfulFragment.newInstance()
+        val fragment = RecoverySuccessfulFragment.newInstance()
         fragment.setCallback(object : BaseCallback {
             override fun callback() {
                 finishAffinity()
                 PresentationManager.stage(this@AccountRecoveryActivity)
             }
         })
-        fragment.show(supportFragmentManager, RecoverySuccesfulFragment.FRAGMENT_KEY)
+        fragment.show(supportFragmentManager, RecoverySuccessfulFragment.FRAGMENT_KEY)
     }
 
-    private fun mnemonicValid(mnemonic: String): Boolean {
-        return mnemonic.split(" ").size == 12
+    private fun mnemonicValid(mnemonic: String) = mnemonic.split(" ").size == 12
+
+    private fun showProgress() {
+
+    }
+
+    private fun hideProgress() {
+
     }
 }
