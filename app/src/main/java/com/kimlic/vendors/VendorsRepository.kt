75e58929e3276d1016a74f529763932446a6c64b
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-08-31 14:21:10 +0300

    add url from qr;

--- app/src/main/java/com/kimlic/vendors/VendorsRepository.kt
+++ app/src/main/java/com/kimlic/vendors/VendorsRepository.kt
@@ -7,15 +7,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.kimlic.API.DoAsync
+import com.kimlic.API.KimlicApi
 import com.kimlic.API.KimlicJSONRequest
 import com.kimlic.API.VolleySingleton
-import com.kimlic.BuildConfig
 import com.kimlic.KimlicApp
 import com.kimlic.db.KimlicDB
 import com.kimlic.db.SyncService
 import com.kimlic.db.entity.VendorDocument
 import com.kimlic.preferences.Prefs
-import com.kimlic.API.KimlicApi
 import com.kimlic.utils.mappers.JsonToVenDocMapper
 import org.json.JSONObject
 import java.io.BufferedReader
@@ -24,24 +23,18 @@ import java.io.InputStreamReader
 
 class VendorsRepository private constructor() {
 
-    // Constants
-
-    private val VENDORS_URL = BuildConfig.VENDORS_URL
-
     // Variables
 
     private var googleSignInAccount = GoogleSignIn.getLastSignedInAccount(KimlicApp.applicationContext())
     private var db: KimlicDB = KimlicDB.getInstance()!!
     private var vendorDao = db.vendorDao()
 
-
     // Public
 
-    fun initDocuments(accountAddress: String, onError: () -> Unit) {
-        val url = VENDORS_URL + KimlicApi.VENDORS.path
+    fun initDocumentsRequest(accountAddress: String, url: String, onError: () -> Unit) {
         val headers = mapOf(Pair("account-address", accountAddress), Pair("accept", "application/vnd.mobile-api.v1+json"))
 
-        val vendorsRequest = KimlicJSONRequest(GET, url, headers, JSONObject(),
+        val vendorsRequest = KimlicJSONRequest(GET, url + KimlicApi.VENDORS.path, headers, JSONObject(),
                 Response.Listener { it ->
                     if (!it.getJSONObject("meta").optString("code").toString().startsWith("2")) {
                         onError()
