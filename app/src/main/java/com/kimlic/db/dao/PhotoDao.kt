12db9550ea49aa9fce05fb9a3f02ffc4b5fec92a
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-04 12:00:46 +0300

    new request;

--- app/src/main/java/com/kimlic/db/dao/PhotoDao.kt
+++ app/src/main/java/com/kimlic/db/dao/PhotoDao.kt
@@ -1,5 +1,6 @@
 package com.kimlic.db.dao
 
+import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.*
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import com.kimlic.db.entity.Photo
@@ -16,14 +17,17 @@ interface PhotoDao {
     @Update
     fun update(photo: Photo): Int
 
-    @Query("SELECT P.id, P.document_id, P.address_id, P.type, P.file, P.inserted_at FROM photo as P INNER JOIN document ON (p.document_id = document.id AND document.type=:documentType )INNER JOIN user ON document.user_id = user.id WHERE user.account_address =:accountAddress")
+    @Query("SELECT P.id, P.document_id, P.address_id, P.user_id, P.type, P.file, P.inserted_at, P.synced FROM photo as P INNER JOIN document ON (p.document_id = document.id AND document.type=:documentType )INNER JOIN user ON document.user_id = user.id WHERE user.account_address =:accountAddress")
     fun selectUserPhotosByDocument(accountAddress: String, documentType: String): List<Photo>
 
-    @Query("SELECT P.id, P.document_id, P.type, P.file, P.inserted_at, P.address_id FROM photo as P INNER JOIN address ON p.address_id = address.id INNER JOIN user ON address.user_id = user.id WHERE user.account_address =:accountAddress")
+    @Query("SELECT P.id, P.document_id, P.address_id, P.user_id, P.type, P.file, P.inserted_at, P.synced FROM photo as P INNER JOIN address ON p.address_id = address.id INNER JOIN user as U ON address.user_id = U.id WHERE U.account_address =:accountAddress")
     fun selectUserAddressPhoto(accountAddress: String): List<Photo>
 
-    @Query("SELECT P.file FROM photo as P INNER JOIN document as D ON P.document_id = D.id INNER JOIN user ON D.user_id = user_id WHERE user.account_address=:accountAddress UNION SELECT portrait_file FROM user WHERE account_address=:accountAddress UNION SELECT portrait_preview_file FROM user WHERE account_address=:accountAddress UNION SELECT p.file FROM photo as P INNER JOIN address as A ON P.address_id = A.id WHERE address_id IN (SELECT address_id FROM user WHERE account_address=:accountAddress)")
+    @Query("SELECT P.file FROM photo as P INNER JOIN document as D ON P.document_id = D.id INNER JOIN user as U ON D.user_id = U.id WHERE account_address=:accountAddress UNION SELECT p.file FROM photo as P INNER JOIN address as A ON P.address_id = A.id WHERE address_id IN (SELECT address_id FROM user WHERE account_address=:accountAddress) UNION SELECT P.file FROM photo as P INNER JOIN user as U WHERE P.type IN('portrait', 'portrait_preview') AND account_address=:accountAddress")
     fun profilePhotos(accountAddress: String): List<String>
+
+    @Query("SELECT P.id, P.document_id, P.address_id, P.user_id, P.type, P.file, P.inserted_at, P.synced FROM photo as P INNER JOIN user as U ON p.user_id = U.id WHERE P.type IN('portrait', 'portrait_preview') AND account_address=:accountAddress")
+    fun portraitPhotosLive(accountAddress: String): LiveData<List<Photo>>
 
     @Delete
     fun delete(photo: Photo)
