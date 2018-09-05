7af53075383685889c779f1c9574401a622cd1be
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-05 11:54:14 +0300

    'unverified' state

--- app/src/main/java/com/kimlic/documents/DocState.kt
+++ app/src/main/java/com/kimlic/documents/DocState.kt
@@ -3,5 +3,6 @@ package com.kimlic.documents
 enum class DocState(val state: String) {
 
     CREATED("Created"),
-    VERIFIED("Verified")
+    VERIFIED("Verified"),
+    UNVERIFIED("Unverified")
 }
