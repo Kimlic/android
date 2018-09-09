84c80e5622c6275861bc587424547f92397c8e11
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-09 14:29:34 +0300

    new;

--- app/src/main/java/com/kimlic/documents/DocumentViewModel.kt
+++ app/src/main/java/com/kimlic/documents/DocumentViewModel.kt
@@ -2,6 +2,8 @@ package com.kimlic.documents
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import com.kimlic.model.ProfileRepository
+import com.kimlic.preferences.Prefs
 import com.kimlic.vendors.VendorsRepository
 
 class DocumentViewModel(application: Application) : AndroidViewModel(application) {
@@ -9,8 +11,11 @@ class DocumentViewModel(application: Application) : AndroidViewModel(application
     // Variables
 
     private val repository = VendorsRepository.instance
+    private val profileRepository = ProfileRepository.instance
 
     // Public
 
     fun countries() = repository.countries()
+
+    fun documents() = profileRepository.documentsLive(Prefs.currentAccountAddress)
 }
