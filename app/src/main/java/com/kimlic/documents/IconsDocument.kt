109cbd6e4217a34fd8016e6fafec7a180f6c2801
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-09 14:31:22 +0300

    new;

---
+++ app/src/main/java/com/kimlic/documents/IconsDocument.kt
@@ -0,0 +1,24 @@
+package com.kimlic.documents
+
+import com.kimlic.R
+
+enum class IconsDocument(val icon: Int) {
+
+    PASSPORT_BLUE(R.drawable.ic_balance_blue_icon),
+    PASSPORT_WHITE(R.drawable.ic_balance_white_icon),
+
+    ID_BLUE(R.drawable.ic_profile_name_icon_blue),
+    ID_WHITE(R.drawable.ic_profile_name_icon_white),
+
+    DRIVERS_LICENSE_BLUE(R.drawable.ic_profile_phone_icon_blue),
+    DRIVERS_LICENSE_WHITE(R.drawable.ic_profile_phone_icon_white),
+
+    RESIDENCE_PERMIT_BLUE(R.drawable.ic_profile_email_icon_blue),
+    RESIDENCE_PERMIT_WHITE(R.drawable.ic_profile_email_icon_white),
+
+    SOCIAL_SECURITY_CARD_BLUE(R.drawable.ic_profile_id_icon_blue),
+    SOCIAL_SECURITY_CARD_WHITE(R.drawable.ic_profile_id_icon_white),
+
+    BIRTH_CERTIFICATE_BLUE(R.drawable.ic_profile_blue_location_icon),
+    BIRTH_CERTIFICATE_WHITE(R.drawable.ic_profile_white_location_icon),
+}
