7f219fc39c86fc1e369d6441683e6f656fb34b9d
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-10 15:22:48 +0300

    add icons;

--- app/src/main/java/com/kimlic/documents/IconsDocument.kt
+++ app/src/main/java/com/kimlic/documents/IconsDocument.kt
@@ -4,21 +4,21 @@ import com.kimlic.R
 
 enum class IconsDocument(val icon: Int) {
 
-    PASSPORT_BLUE(R.drawable.ic_balance_blue_icon),
-    PASSPORT_WHITE(R.drawable.ic_balance_white_icon),
+    PASSPORT_BLUE(R.drawable.ic_passport_blue),
+    PASSPORT_WHITE(R.drawable.ic_passport_white),
 
-    ID_BLUE(R.drawable.ic_profile_name_icon_blue),
-    ID_WHITE(R.drawable.ic_profile_name_icon_white),
+    ID_BLUE(R.drawable.ic_id_card_blue),
+    ID_WHITE(R.drawable.ic_id_card_white),
 
-    DRIVERS_LICENSE_BLUE(R.drawable.ic_profile_phone_icon_blue),
-    DRIVERS_LICENSE_WHITE(R.drawable.ic_profile_phone_icon_white),
+    DRIVERS_LICENSE_BLUE(R.drawable.ic_car_blue),
+    DRIVERS_LICENSE_WHITE(R.drawable.ic_id_card_white),
 
-    RESIDENCE_PERMIT_BLUE(R.drawable.ic_profile_email_icon_blue),
-    RESIDENCE_PERMIT_WHITE(R.drawable.ic_profile_email_icon_white),
+    RESIDENCE_PERMIT_BLUE(R.drawable.ic_house_blue),
+    RESIDENCE_PERMIT_WHITE(R.drawable.ic_house_white),
 
-    SOCIAL_SECURITY_CARD_BLUE(R.drawable.ic_profile_id_icon_blue),
-    SOCIAL_SECURITY_CARD_WHITE(R.drawable.ic_profile_id_icon_white),
+    SOCIAL_SECURITY_CARD_BLUE(R.drawable.ic_social_securty_card_blue),
+    SOCIAL_SECURITY_CARD_WHITE(R.drawable.ic_social_securty_card_white),
 
-    BIRTH_CERTIFICATE_BLUE(R.drawable.ic_profile_blue_location_icon),
-    BIRTH_CERTIFICATE_WHITE(R.drawable.ic_profile_white_location_icon),
+    BIRTH_CERTIFICATE_BLUE(R.drawable.ic_stroller_blue),
+    BIRTH_CERTIFICATE_WHITE(R.drawable.ic_stroller_white),
 }
