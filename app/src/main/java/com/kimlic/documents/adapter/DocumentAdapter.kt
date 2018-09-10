de9041a8ab5815952c9f35f694658a37d2ac0e77
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-10 16:41:51 +0300

    refactoring;

--- app/src/main/java/com/kimlic/documents/adapter/DocumentAdapter.kt
+++ app/src/main/java/com/kimlic/documents/adapter/DocumentAdapter.kt
@@ -1,12 +1,13 @@
 package com.kimlic.documents.adapter
 
+import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import com.kimlic.R
-import com.kimlic.documents.IconsDocument
+import com.kimlic.documents.DocIcons
 import com.kimlic.utils.AppDoc
 import kotlinx.android.synthetic.main.item_document.view.*
 
@@ -16,46 +17,49 @@ class DocumentAdapter : BaseAdapter() {
 
     private var documents: List<String> = emptyList()
     var selectedPosition: Int = -1
+    private lateinit var context: Context
 
     // Life
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val context = parent!!.context
-        val view: View = convertView ?: LayoutInflater.from(parent.context).inflate(R.layout.item_document, parent, false)
+        context = parent!!.context
+        val view: View = convertView ?: LayoutInflater.from(context).inflate(R.layout.item_document, parent, false)
+        val checked = position == selectedPosition
 
-        view.choisIv.visibility = View.INVISIBLE
-        view.documentTv.setTextColor(context.getColor(R.color.lightBlue))
+        with(view) {
+            choisIv.visibility = View.INVISIBLE
+            documentTv.setTextColor(context.getColor(R.color.lightBlue))
 
-        when (documents[position]) {
-            AppDoc.PASSPORT.type -> {
-                view.documentTv.text = context.resources.getString(R.string.passport); view.tag = AppDoc.PASSPORT.type
-                view.documentIv.background = context.resources.getDrawable(IconsDocument.BIRTH_CERTIFICATE_BLUE.icon, null)
+            when (documents[position]) {
+                AppDoc.PASSPORT.type -> {
+                    documentTv.text = context.resources.getString(R.string.passport); tag = AppDoc.PASSPORT.type
+                    documentIv.background = if (!checked) icon(DocIcons.PASSPORT_BLUE.icon) else icon(DocIcons.PASSPORT_WHITE.icon)
+                }
+                AppDoc.ID_CARD.type -> {
+                    documentTv.text = context.resources.getString(R.string.id_card); tag = AppDoc.ID_CARD.type
+                    documentIv.background = if (!checked) icon(DocIcons.ID_BLUE.icon) else icon(DocIcons.ID_WHITE.icon)
+                }
+                AppDoc.DRIVERS_LICENSE.type -> {
+                    documentTv.text = context.resources.getString(R.string.drivers_license); tag = AppDoc.DRIVERS_LICENSE
+                    documentIv.background = if (!checked) icon(DocIcons.DRIVERS_LICENSE_BLUE.icon) else icon(DocIcons.DRIVERS_LICENSE_WHITE.icon)
+                }
+                AppDoc.RESIDENCE_PERMIT_CARD.type -> {
+                    documentTv.text = context.getString(R.string.residence_permit); tag = AppDoc.RESIDENCE_PERMIT_CARD.type
+                    documentIv.background = if (!checked) icon(DocIcons.RESIDENCE_PERMIT_BLUE.icon) else icon(DocIcons.RESIDENCE_PERMIT_WHITE.icon)
+                }
+                AppDoc.SOCIAL_SECURITY_CARD.type -> {
+                    documentTv.text = context.getString(R.string.social_security_card); tag = AppDoc.SOCIAL_SECURITY_CARD
+                    documentIv.background = if (!checked) icon(DocIcons.SOCIAL_SECURITY_CARD_BLUE.icon) else icon(DocIcons.SOCIAL_SECURITY_CARD_WHITE.icon)
+                }
+                AppDoc.BIRTH_CERTIFICATE.type -> {
+                    documentTv.text = context.getString(R.string.birth_certificate); tag = AppDoc.BIRTH_CERTIFICATE
+                    documentIv.background = if (!checked) icon(DocIcons.BIRTH_CERTIFICATE_BLUE.icon) else icon(DocIcons.BIRTH_CERTIFICATE_WHITE.icon)
+                }
             }
-            AppDoc.ID_CARD.type -> {
-                view.documentTv.text = context.resources.getString(R.string.id_card); view.tag = AppDoc.ID_CARD.type
-                view.documentIv.background = context.resources.getDrawable(IconsDocument.ID_BLUE.icon, null)
-            }
-            AppDoc.DRIVERS_LICENSE.type -> {
-                view.documentTv.text = context.resources.getString(R.string.drivers_license); view.tag = AppDoc.DRIVERS_LICENSE
-                view.documentIv.background = context.resources.getDrawable(IconsDocument.DRIVERS_LICENSE_BLUE.icon, null)
-            }
-            AppDoc.RESIDENCE_PERMIT_CARD.type -> {
-                view.documentTv.text = context.getString(R.string.residence_permit); view.tag = AppDoc.RESIDENCE_PERMIT_CARD.type
-                view.documentIv.background = context.resources.getDrawable(IconsDocument.RESIDENCE_PERMIT_BLUE.icon, null)
-            }
-            AppDoc.SOCIAL_SECURITY_CARD.type -> {
-                view.documentTv.text = context.getString(R.string.social_security_card); view.tag = AppDoc.SOCIAL_SECURITY_CARD
-                view.documentIv.background = context.resources.getDrawable(IconsDocument.SOCIAL_SECURITY_CARD_BLUE.icon, null)
-            }
-            AppDoc.BIRTH_CERTIFICATE.type -> {
-                view.documentTv.text = context.getString(R.string.birth_certificate); view.tag = AppDoc.BIRTH_CERTIFICATE
-                view.documentIv.background = context.resources.getDrawable(IconsDocument.DRIVERS_LICENSE_BLUE.icon, null)
-            }
-        }
 
-        if (position == selectedPosition) {
-            view.choisIv.visibility = View.VISIBLE
-            view.documentTv.setTextColor(Color.WHITE)
+            if (checked) {
+                choisIv.visibility = View.VISIBLE; documentTv.setTextColor(Color.WHITE)
+            }
         }
 
         return view
@@ -73,4 +77,8 @@ class DocumentAdapter : BaseAdapter() {
         this.documents = documents!!
         notifyDataSetChanged()
     }
+
+    // Private
+
+    private fun icon(icon: Int) = context.resources.getDrawable(icon, null)
 }
