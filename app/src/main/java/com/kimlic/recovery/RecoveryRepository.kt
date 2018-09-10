1d08169e710e9119e37a87bf664777578755cdf9
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-10 12:11:10 +0300

    refactoring;

--- app/src/main/java/com/kimlic/recovery/RecoveryRepository.kt
+++ app/src/main/java/com/kimlic/recovery/RecoveryRepository.kt
@@ -44,7 +44,7 @@ class RecoveryRepository {
 
     fun backupDatabase(accountAddress: String, onSuccess: () -> Unit, onError: () -> Unit) {
         SyncService.getInstance().backupDatabase(accountAddress, "kimlic.db", onSuccess, onError)
-//        DoAsync().execute(Runnable { SyncService.getInstance().backupDatabase(accountAddress, "kimlic.db", onSuccess, onError) })
+//        DoAsync().execute(Runnable { SyncService.getInstance().backupDatabase(ACCOUNT_ADDRESS, "kimlic.db", onSuccess, onError) })
     }
 
     fun backupPhoto(accountAddress: String, filePath: String, onSuccess: () -> Unit, onError: () -> Unit) {
