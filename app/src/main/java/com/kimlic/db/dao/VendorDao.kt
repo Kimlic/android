a692b94d4bb64d74d767d94344bbf1ed58922207
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-13 09:29:19 +0300

    new query delete;

--- app/src/main/java/com/kimlic/db/dao/VendorDao.kt
+++ app/src/main/java/com/kimlic/db/dao/VendorDao.kt
@@ -1,10 +1,7 @@
 package com.kimlic.db.dao
 
 import android.arch.lifecycle.LiveData
-import android.arch.persistence.room.Dao
-import android.arch.persistence.room.Insert
-import android.arch.persistence.room.OnConflictStrategy
-import android.arch.persistence.room.Query
+import android.arch.persistence.room.*
 import com.kimlic.db.entity.VendorDocument
 
 @Dao
@@ -19,5 +16,6 @@ interface VendorDao {
     @Query("SELECT * FROM vendor_document")
     fun select(): List<VendorDocument>
 
-
+    @Query("DELETE from vendor_document")
+    fun deleteAll()
 }
