2bf93c5c6d9148dced08661cf938477a0a3acd31
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-16 16:38:47 +0300

    refactoring;

--- app/src/main/java/com/kimlic/vendors/VendorsViewModel.kt
+++ app/src/main/java/com/kimlic/vendors/VendorsViewModel.kt
@@ -14,59 +14,32 @@ class VendorsViewModel : ViewModel(), LifecycleObserver {
     // Variables
 
     private val vendorsRepository = VendorsRepository.instance
-    private val profileRepository = ProfileRepository.instance
-
-    private var vendorSupportedDocuments = object : MutableLiveData<List<Document>>() {}
     private var vendorRequestStatus: SingleLiveEvent<String> = SingleLiveEvent()
     private val timeQueue = ArrayDeque<Long>(listOf(4000L, 5000L, 10000L))
 
     // Public
 
-    fun documentsListRequest(url: String) = vendorsRepository.initDocumentsRequest(Prefs.currentAccountAddress, url = url, onError = { retry(url) })
-
+    /*
+    * Clear previous information about vendor documents
+    * */
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun clearVendors() = vendorsRepository.clearVendorsDocs()
 
-    // New Requests
-    fun RPDocuments(url: String) = vendorsRepository.rpDocumentsRequest(accountAddress = Prefs.currentAccountAddress, url = url, onError = { retryRpRequest(url) })
+    /*
+    * Request to RP. Url gets from QR code. Repository saves Data to DB
+    * */
+
+    fun rpDocuments(url: String) = vendorsRepository.rpDocumentsRequest(Prefs.currentAccountAddress, url, onError = { retryRpRequest(url) })
 
     fun vendorsDocumentsToVerify() = vendorsRepository.vendorDocumentsLive()
 
     fun vendorRequestStatus() = vendorRequestStatus
 
-    fun getDocumentsForAdapter() = vendorSupportedDocuments// List Document which need current RP
-
-    fun supportedDocuments(country: String) {
-        val userDocuments = profileRepository.documents(Prefs.currentAccountAddress)
-        val linkedDocs = userDocuments.map { it.type to it }.toMap()
-
-        // Documents are supported in chosen country
-        val supportedDocuments = mutableListOf<Document>()
-        val vendorsDocuments = vendorsRepository.vendorDocuments()
-
-        vendorsDocuments.forEach { document ->
-            if (document.countries.contains(country.toUpperCase())) {
-                when (document.type) {
-                    "ID_CARD" -> supportedDocuments.add(Document(type = AppDoc.ID_CARD.type))
-                    "PASSPORT" -> supportedDocuments.add(Document(type = AppDoc.PASSPORT.type))
-                    "DRIVERS_LICENSE" -> supportedDocuments.add(Document(type = AppDoc.DRIVERS_LICENSE.type))
-                    "RESIDENCE_PERMIT_CARD" -> supportedDocuments.add(Document(type = AppDoc.RESIDENCE_PERMIT_CARD.type))
-                }
-            }
-        }
-
-        vendorSupportedDocuments.postValue(supportedDocuments)
-    }
-
     fun countries() = vendorsRepository.countries()
 
     // Private
 
-    private fun retry(url: String) {
-        timeQueue.poll()?.let { Handler().postDelayed({ documentsListRequest(url) }, it) } ?: vendorRequestStatus.postValue("server error")
-    }
-
     private fun retryRpRequest(url: String) {
-        timeQueue.poll()?.let { Handler().postDelayed({ RPDocuments(url) }, it) } ?: vendorRequestStatus.postValue("server error")
+        timeQueue.poll()?.let { Handler().postDelayed({ rpDocuments(url) }, it) } ?: vendorRequestStatus.postValue("server error")
     }
 }
