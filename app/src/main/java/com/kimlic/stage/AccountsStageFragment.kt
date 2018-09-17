b82f850de4a8086ffb3f7b17eb0525115c357bd6
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-17 15:38:57 +0300

    add account adapter;

--- app/src/main/java/com/kimlic/stage/AccountsStageFragment.kt
+++ app/src/main/java/com/kimlic/stage/AccountsStageFragment.kt
@@ -1,11 +1,17 @@
 package com.kimlic.stage
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.kimlic.BaseFragment
 import com.kimlic.R
+import com.kimlic.stage.adapter.AccountAdapter
+import com.kimlic.stage.adapter.OnAccountItemClick
+import kotlinx.android.synthetic.main.fragment_stage_accounts.*
 
 class AccountsStageFragment : BaseFragment() {
 
@@ -21,6 +27,10 @@ class AccountsStageFragment : BaseFragment() {
         }
     }
 
+    // Variables
+
+    private lateinit var adapter: AccountAdapter
+
     // Life
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -35,5 +45,18 @@ class AccountsStageFragment : BaseFragment() {
     // Private
 
     private fun setupUI() {
+        adapter = AccountAdapter()
+        adapter.setOnAccountItemClick(object : OnAccountItemClick {
+            override fun onClick(position: Int) {
+                Log.d("TAGCLICK", "position ++")
+            }
+        })
+
+        recycler.layoutManager = LinearLayoutManager(activity, LinearLayout.VERTICAL, false)
+        recycler.adapter = adapter
+
+        val mockList = listOf("Kimlic", "Ebay", "Amazon", "EXMP", "BP", "AWS", "MacBookPro", "Xiaomi")
+        adapter.setAccountsList(mockList)
+
     }
 }
