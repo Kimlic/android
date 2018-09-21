51dd992138e6ec56b024923f814b1ec3500d0426
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-21 12:29:18 +0300

    refactoring;

--- app/src/main/java/com/kimlic/camera/camera2basic/CameraActivity.kt
+++ app/src/main/java/com/kimlic/camera/camera2basic/CameraActivity.kt
@@ -24,7 +24,7 @@ class CameraActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camera)
+        //setContentView(R.layout.activity_camera)
         savedInstanceState ?: supportFragmentManager.beginTransaction()
                 .replace(R.id.container, Camera2BasicFragment.newInstance())
                 .commit()
