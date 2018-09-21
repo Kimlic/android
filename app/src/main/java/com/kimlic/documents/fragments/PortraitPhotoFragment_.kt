afeb09760172feebc8fc85ebe990b68b394c5ecf
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-21 11:43:12 +0300

    new test;

---
+++ app/src/main/java/com/kimlic/documents/fragments/PortraitPhotoFragment_.kt
@@ -0,0 +1,74 @@
+package com.kimlic.documents.fragments
+
+import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.StyleSpan
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kimlic.R
+import com.kimlic.camera.CameraBaseFragment_
+import com.kimlic.utils.AppConstants
+import kotlinx.android.synthetic.main.fragment_document_portrait.*
+
+class PortraitPhotoFragment_ : CameraBaseFragment_() {
+
+    // Companion
+
+    companion object {
+        val FRAGMENT_KEY = this::class.java.simpleName!!
+
+        fun newInstance(bundle: Bundle = Bundle()): PortraitPhotoFragment_ {
+            val fragment = PortraitPhotoFragment_()
+            bundle.putInt(AppConstants.CAMERA_TYPE.key, AppConstants.CAMERA_FRONT.intKey)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    // Life
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_document_portrait_, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupUI()
+    }
+
+    // Private
+
+    private fun setupUI() {
+        val action = arguments?.getString("action", "")
+
+        when (action) {
+            AppConstants.PORTRAIT_DOCUMENT.key -> {
+                setDocumentBackground()
+            }
+        }
+
+        backBt.setOnClickListener { activity!!.finish() }
+        setupTitle()
+    }
+
+    private fun setupTitle() {
+        val spanText = getString(R.string.take_your_portrait_photo)
+        val words = spanText.split(" ")
+        val spanStart = words[0].length + words[1].length + 2
+        val spannableBuilder = SpannableStringBuilder(spanText)
+        val boldStyle = StyleSpan(android.graphics.Typeface.BOLD)
+
+        spannableBuilder.setSpan(boldStyle, spanStart, spanText.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        documenTitleTv.text = spannableBuilder
+    }
+
+    private fun setDocumentBackground() {
+        auxilaryContourIv.setBackgroundResource(R.drawable.ic_id_validation_white_contour_and_mask_bakcground)
+        val layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT)
+        auxilaryContourIv.layoutParams = layoutParams
+    }
+}
