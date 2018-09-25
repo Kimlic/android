20e5683234a96995e9c46a196f4c8d0d6906e98f
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-25 11:23:06 +0300

    add sync request on message;

--- app/src/main/java/com/kimlic/firebase/KimlicMessagingService.kt
+++ app/src/main/java/com/kimlic/firebase/KimlicMessagingService.kt
@@ -1,27 +1,26 @@
 package com.kimlic.firebase
 
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.kimlic.model.ProfileRepository
+import com.kimlic.preferences.Prefs
 
 class KimlicMessagingService : FirebaseMessagingService() {
 
     // Variables
+
+    private var profileRepository: ProfileRepository? = ProfileRepository.instance
 
     // Live
 
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
         super.onMessageReceived(remoteMessage)
 
-
-//        Thread(Runnable {
-//            Looper.prepare()
-//            val profileRepository = ProfileRepository.instance; profileRepository.syncProfile(Prefs.currentAccountAddress) }).start()
-
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d("TAGFIREBASE", "From: ${remoteMessage?.getFrom()}");
+        val handler = Handler(Looper.getMainLooper())
+        handler.post { ProfileRepository.instance.syncProfile(Prefs.currentAccountAddress) }
 
         // Check if message contains a data payload.
         if (remoteMessage?.getData()?.size != 0) {
@@ -44,5 +43,10 @@ class KimlicMessagingService : FirebaseMessagingService() {
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        profileRepository = null
     }
 }
