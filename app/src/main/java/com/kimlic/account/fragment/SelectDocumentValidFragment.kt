35d4155f2ef18dd68a88bdc8fd7150ca47cc7773
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-28 17:47:09 +0300

    show documents in sorted flow;

--- app/src/main/java/com/kimlic/account/fragment/SelectDocumentValidFragment.kt
+++ app/src/main/java/com/kimlic/account/fragment/SelectDocumentValidFragment.kt
@@ -8,12 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.kimlic.BasePopupFragment
 import com.kimlic.R
+import com.kimlic.account.AccountActivity
 import com.kimlic.account.SelectAccountDocumentViewModel
 import com.kimlic.account.adapter.DocumentValidAdapter
 import com.kimlic.db.entity.Document
 import com.kimlic.db.entity.VendorDocument
 import com.kimlic.documents.DocumentViewModel
 import com.kimlic.managers.PresentationManager
+import com.kimlic.model.ProfileViewModel
+import com.kimlic.utils.AppDoc
 import com.kimlic.vendors.VendorsViewModel
 import kotlinx.android.synthetic.main.fragment_select_document_valid.*
 
@@ -35,12 +38,14 @@ class SelectDocumentValidFragment : BasePopupFragment() {
 
     private lateinit var documentModel: DocumentViewModel
     private lateinit var vendorsModel: VendorsViewModel
+    private lateinit var model: ProfileViewModel
     private lateinit var accountDocumentModel: SelectAccountDocumentViewModel
 
     private lateinit var vendorsDocumentsList: List<VendorDocument>
-    private lateinit var userDocumentsList: List<Document>
+    //private lateinit var userDocumentsList: List<Document>
     private lateinit var documentAccountAdapter: DocumentValidAdapter
 
+    //    private var adaptersList: MutableList<ValidDocument> = mutableListOf()
     private var adaptersList: MutableList<ValidDocument> = mutableListOf()
 
     // Life
@@ -54,6 +59,8 @@ class SelectDocumentValidFragment : BasePopupFragment() {
         accountDocumentModel = ViewModelProviders.of(this).get(SelectAccountDocumentViewModel::class.java)
         documentModel = ViewModelProviders.of(this).get(DocumentViewModel::class.java)
         vendorsModel = ViewModelProviders.of(this).get(VendorsViewModel::class.java)
+        model = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+
         setupUI()
     }
 
@@ -68,23 +75,53 @@ class SelectDocumentValidFragment : BasePopupFragment() {
         documentAccountAdapter = DocumentValidAdapter()
         listView.adapter = documentAccountAdapter
 
-        documentModel.documentsLive().observe(this, Observer<List<Document>> { userDocuments ->
-            userDocumentsList = userDocuments.orEmpty()
+//        documentModel.documentsLive().observe(this, Observer<List<Document>> { userDocuments ->
+//            userDocumentsList = userDocuments.orEmpty()
+//            vendorsDocumentsList = vendorsModel.vendorsDocuments()
+//            adaptersList = mutableListOf()
+//
+//            val sortedDocTypes = AppDoc.values().map { it.type }.toList()
+//
+//            val vendorsList = vendorsDocumentsList.map { it.type to it.countries }.toMap()
+
+//            userDocumentsList.forEach {
+//                adaptersList.add(ValidDocument(it, it.type in vendorsList.keys))
+//            }
+//            documentAccountAdapter.setDocuments(adaptersList)
+//        })
+
+        ViewModelProviders.of(this).get(ProfileViewModel::class.java).userDocumentsLive().observe(this, Observer<List<Document>> { userDocumentsList ->
+            val userDocumentsMap = userDocumentsList?.map { it.type to it }?.toMap().orEmpty()
+            val userDocumentsPresentTypes = userDocumentsList?.map { it.type }!!.toList()
             vendorsDocumentsList = vendorsModel.vendorsDocuments()
+
+            val sortedDocTypes = AppDoc.values().map { it.type }.toList()
+            val vendorTypes = vendorsDocumentsList.map { it.type }
+            val vendorTypeContries = vendorsDocumentsList.map { it.type to it.countries }.toMap()
+
             adaptersList = mutableListOf()
 
-            val vendorsList = vendorsDocumentsList.map { it.type to it.countries }.toMap()
-
-            userDocumentsList.forEach {
-                adaptersList.add(ValidDocument(it, it.type in vendorsList.keys))
-
+            sortedDocTypes.forEach {
+                if (it in userDocumentsPresentTypes) {
+                    if (userDocumentsMap[it]!!.type in vendorTypes) {
+                        if (model.userDocument(it)!!.countryIso.toUpperCase() in vendorTypeContries[it]!!) {
+                            adaptersList.add(ValidDocument(model.userDocument(it)!!, true))
+                        } else adaptersList.add(ValidDocument(model.userDocument(it)!!, false))
+                    }
+                }
             }
             documentAccountAdapter.setDocuments(adaptersList)
+
         })
 
-
         listView.setOnItemClickListener { _, view, position, _ ->
-            if (adaptersList[position].isValid) PresentationManager.detailsDocumentChoose(activity!!, adaptersList[position].document.type, 4411)
+            // Previous flow
+            //if (adaptersList[position].isValid) PresentationManager.detailsDocumentChoose(activity!!, adaptersList[position].document.type, 4411)
+
+            if (adaptersList[position].isValid) {
+                (activity as AccountActivity).setChosenDocument(adaptersList[position].document.type)
+                dismiss()
+            } //PresentationManager.detailsDocumentChoose(activity!!, adaptersList[position].document.type, 4411)
         }
 
         addDocumentBt.setOnClickListener { _ ->
