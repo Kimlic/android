240bbc953f4736dc9ffbc807ff387253a78b2442
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-09-29 16:50:46 +0300

    refactoring;

--- app/src/main/java/com/kimlic/db/dao/CompanyDao.kt
+++ app/src/main/java/com/kimlic/db/dao/CompanyDao.kt
@@ -14,13 +14,13 @@ interface CompanyDao {
     fun update(company: Company)
 
     @Query("SELECT * FROM company WHERE id =:id")
-    fun account(id: String): Company
+    fun company(id: String): Company
 
     @Query("SELECT C.id, C.user_id, C.address, C.details, C.email, C.logo, C. name, C.phone, C.website, C.status, C.inserted_at  FROM company as C INNER JOIN user as U ON user_id = U.id WHERE u.account_address=:accountAddress")
-    fun accounts(accountAddress: String): List<Company>
+    fun companies(accountAddress: String): List<Company>
 
     @Query("SELECT C.id, C.user_id, C.address, C.details, C.email, C.logo, C. name, C.phone, C.website, C.status, C.inserted_at FROM company as C INNER JOIN user as U ON user_id = U.id WHERE u.account_address=:accountAddress")
-    fun accountsLive(accountAddress: String): LiveData<List<Company>>
+    fun companiesLive(accountAddress: String): LiveData<List<Company>>
 
     @Delete
     fun delete(company: Company)
