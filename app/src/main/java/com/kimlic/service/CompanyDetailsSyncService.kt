31e5d920194613afe7ed04fa8ccc0459515e8a5b
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-10-01 18:19:03 +0300

    CompanyDetailsService;

---
+++ app/src/main/java/com/kimlic/service/CompanyDetailsSyncService.kt
@@ -0,0 +1,61 @@
+package com.kimlic.service
+
+import android.app.IntentService
+import android.content.Intent
+import android.util.Log
+import com.android.volley.Request.Method.GET
+import com.android.volley.Response
+import com.kimlic.API.KimlicApi
+import com.kimlic.API.KimlicJSONRequest
+import com.kimlic.API.VolleySingleton
+import com.kimlic.db.KimlicDB
+import com.kimlic.db.dao.CompanyDao
+import com.kimlic.db.entity.Company
+import com.kimlic.preferences.Prefs
+import com.kimlic.utils.AppConstants
+import org.json.JSONObject
+import java.util.*
+
+class CompanyDetailsSyncService : IntentService("DetailsSyncService") {
+
+    // Variables
+
+    private var dataBase: KimlicDB? = null
+    private lateinit var companyDao: CompanyDao
+    private lateinit var unverifiedList: List<Company>
+    private lateinit var unverifiedQueue: ArrayDeque<Company>
+    // Live
+
+    override fun onCreate() {
+        super.onCreate()
+
+        dataBase = KimlicDB.getInstance()
+        companyDao = dataBase!!.companyDao()
+        unverifiedQueue = ArrayDeque()
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+        unverifiedList = companyDao.companyByStatus(Prefs.currentAccountAddress, AppConstants.UNVERIFIED.key)
+        unverifiedQueue.addAll(unverifiedList)
+        Log.d("TAGSERVICE", "unverified list ${unverifiedList}")
+        recursiveRequest()
+    }
+
+    // Private
+
+    private fun recursiveRequest() {
+        unverifiedQueue.poll()?.let {
+            val headers = mapOf(Pair("account-address", Prefs.currentAccountAddress), Pair("accept", "application/vnd.mobile-api.v1+json"))
+
+            val request = KimlicJSONRequest(GET, it.website + KimlicApi.DOCUMENTS_VERIFIED, headers, JSONObject(),
+                    Response.Listener {
+                        // TODO parce responce, update database!!!
+                    },
+                    Response.ErrorListener {
+                        Log.d("TAGSERVICE", "sync responce - $it")
+                    })
+
+            VolleySingleton.getInstance(this).requestQueue.add(request)
+        } ?: stopSelf()
+    }
+}
