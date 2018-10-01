c0d89d54f37d40cf026ee88a19fb6cd347853297
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-10-01 12:04:30 +0300

    use Glide instead Picasso;

--- app/src/main/java/com/kimlic/account/adapter/DocumentValidAdapter.kt
+++ app/src/main/java/com/kimlic/account/adapter/DocumentValidAdapter.kt
@@ -5,10 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import com.bumptech.glide.Glide
 import com.kimlic.R
 import com.kimlic.account.fragment.SelectDocumentValidFragment
 import com.kimlic.utils.AppDoc
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_document_small_valid.view.*
 
 class DocumentValidAdapter : BaseAdapter() {
@@ -45,7 +45,7 @@ class DocumentValidAdapter : BaseAdapter() {
                 documentTv.setTextColor(context.getColor(android.R.color.white))
             }
             choisIv.visibility = View.GONE// TODO remove
-            Picasso.get().load(path).noFade().into(flagIv)
+            Glide.with(view).load(path).into(flagIv)
         }
 
         return view
