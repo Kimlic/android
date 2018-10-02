a0ae0cf9d3ffc0726083e5fe19db1847ae88139c
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-10-02 18:56:58 +0300

    refactoring;

--- app/src/main/java/com/kimlic/db/dao/CompanyDao.kt
+++ app/src/main/java/com/kimlic/db/dao/CompanyDao.kt
@@ -16,16 +16,16 @@ interface CompanyDao {
     @Query("SELECT * FROM company WHERE id =:id")
     fun company(id: String): Company
 
-    @Query("SELECT C.id, C.user_id, C.address, C.details, C.email, C.logo, C. name, C.phone, C.website, C.status, C.inserted_at  FROM company as C INNER JOIN user as U ON user_id = U.id AND status=:status WHERE u.account_address=:accountAddress")
+    @Query("SELECT C.id, C.user_id, C.address, C.details, C.email, C.logo, C. name, C.phone, C.website, C.status, C.inserted_at, C.verified_at  FROM company as C INNER JOIN user as U ON user_id = U.id AND status=:status WHERE u.account_address=:accountAddress")
     fun companyByStatus(accountAddress: String, status: String): List<Company>
 
-    @Query("SELECT C.id, C.user_id, C.address, C.details, C.email, C.logo, C. name, C.phone, C.website, C.status, C.inserted_at  FROM company as C INNER JOIN user as U ON user_id = U.id WHERE u.account_address=:accountAddress")
+    @Query("SELECT C.id, C.user_id, C.address, C.details, C.email, C.logo, C. name, C.phone, C.website, C.status, C.inserted_at, C.verified_at  FROM company as C INNER JOIN user as U ON user_id = U.id WHERE u.account_address=:accountAddress")
     fun companies(accountAddress: String): List<Company>
 
     @Query("SELECT C.id  FROM company as C INNER JOIN user as U ON user_id = U.id WHERE u.account_address=:accountAddress")
     fun companyIds(accountAddress: String): List<String>
 
-    @Query("SELECT C.id, C.user_id, C.address, C.details, C.email, C.logo, C. name, C.phone, C.website, C.status, C.inserted_at FROM company as C INNER JOIN user as U ON user_id = U.id WHERE u.account_address=:accountAddress")
+    @Query("SELECT C.id, C.user_id, C.address, C.details, C.email, C.logo, C. name, C.phone, C.website, C.status, C.inserted_at, C.verified_at FROM company as C INNER JOIN user as U ON user_id = U.id WHERE u.account_address=:accountAddress")
     fun companiesLive(accountAddress: String): LiveData<List<Company>>
 
     @Delete
