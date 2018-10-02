b28c2864891004ab5c364674661b6afa878a1466
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-10-02 16:26:19 +0300

    new functions;

--- app/src/main/java/com/kimlic/stage/CompanyDetailsViewModel.kt
+++ app/src/main/java/com/kimlic/stage/CompanyDetailsViewModel.kt
@@ -3,6 +3,8 @@ package com.kimlic.stage
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import com.kimlic.account.CompanyRepository
+import com.kimlic.db.entity.Company
+import com.kimlic.model.ProfileRepository
 import com.kimlic.preferences.Prefs
 
 class CompanyDetailsViewModel(application: Application) : AndroidViewModel(application) {
@@ -10,10 +12,19 @@ class CompanyDetailsViewModel(application: Application) : AndroidViewModel(appli
     // Variables
 
     private val companyRepository = CompanyRepository.instance
+    private var profileRepository = ProfileRepository.instance
 
     // Public
+
+    fun saveCompany(company: Company) {
+        val userId = profileRepository.getUser(Prefs.currentAccountAddress).id
+        company.userId = userId
+        companyRepository.saveCompany(company)
+    }
 
     fun companiesLive() = companyRepository.companiesLive(Prefs.currentAccountAddress)
 
     fun company(companyId: String) = companyRepository.company(companyId)
+
+    fun companyIds() = companyRepository.companyIds(Prefs.currentAccountAddress)
 }
