9cd2627637306a1e98b00c9f650ffbf93e2b1858
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-10-02 19:24:36 +0300

    refactoring;

--- app/src/main/java/com/kimlic/service/CompanyDetailsSyncService.kt
+++ app/src/main/java/com/kimlic/service/CompanyDetailsSyncService.kt
@@ -14,6 +14,7 @@ import com.kimlic.db.KimlicDB
 import com.kimlic.db.dao.CompanyDao
 import com.kimlic.db.entity.Company
 import com.kimlic.preferences.Prefs
+import com.kimlic.service.entity.CompanyVerifyData
 import com.kimlic.service.entity.CompanyVerifyEntity
 import com.kimlic.utils.AppConstants
 import org.json.JSONObject
@@ -48,16 +49,17 @@ class CompanyDetailsSyncService : IntentService("DetailsSyncService") {
 
     private fun recursiveRequest() {
         unverifiedQueue.poll()?.let {
-            val headers = mapOf(Pair("account-address", Prefs.currentAccountAddress), Pair("accept", "application/vnd.mobile-api.v1+json"))
+            val headers = mapOf(Pair("account-address", Prefs.currentAccountAddress), Pair("accept", "application/vnd.mobile-api.v1+json"), Pair("Content-Type", "application/json"))
 
-            val request = KimlicJSONRequest(GET, it.website + KimlicApi.DOCUMENTS_VERIFIED.path, headers, JSONObject(),
+//            val request = KimlicJSONRequest(GET, it.website + KimlicApi.DOCUMENTS_VERIFIED.path, headers, JSONObject(),
+            val request = KimlicJSONRequest(GET, "http://13.68.143.152:4002" + KimlicApi.DOCUMENTS_VERIFIED.path, headers, JSONObject(),
                     Response.Listener {
                         // TODO parce responce, update database!!!
-                        val type = object : TypeToken<CompanyVerifyEntity>() {}.type
-                        val documentDataString = it.getJSONObject("data").getJSONArray("document").toString()
+                        val type = object : TypeToken<CompanyVerifyData>() {}.type
+                        //val documentDataString = it.getJSONArray("data").optString(0).toString()//.getJSONObject("document").toString()
 
-                        val verifiedDocumentInfo: CompanyVerifyEntity = Gson().fromJson(documentDataString, type)
-                        Log.d("TAGSERVICE", "company verify entity - $it")
+                        //val verifiedDocumentInfo: CompanyVerifyEntity = Gson().fromJson(documentDataString, type)
+                        //Log.d("TAGSERVICE", "company verify entity - ${verifiedDocumentInfo.verifiedAt}")
                     },
                     Response.ErrorListener {
                         Log.d("TAGSERVICE", "sync responce - $it")
