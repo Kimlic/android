8836fefaa6e511e3d6787045ba5f8b010109c01a
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-10-06 13:47:46 +0300

    refactoring;

--- app/src/main/java/com/kimlic/db/entity/CompanyDocumentJoin.kt
+++ app/src/main/java/com/kimlic/db/entity/CompanyDocumentJoin.kt
@@ -1,23 +1,19 @@
 package com.kimlic.db.entity
 
-import android.arch.persistence.room.ColumnInfo
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.ForeignKey
-import android.arch.persistence.room.Ignore
+import android.arch.persistence.room.*
+import android.arch.persistence.room.ForeignKey.CASCADE
 
 @Entity(tableName = "company_document_join",
         primaryKeys = ["company_id", "document_id"],
         foreignKeys = [
-            ForeignKey(entity = Company::class, parentColumns = ["id"], childColumns = ["company_id"]),
-            ForeignKey(entity = Document::class, parentColumns = ["id"], childColumns = ["document_id"])
+            ForeignKey(entity = Company::class, parentColumns = ["id"], childColumns = ["company_id"], onDelete = CASCADE, onUpdate = CASCADE),
+            ForeignKey(entity = Document::class, parentColumns = ["id"], childColumns = ["document_id"], onDelete = CASCADE, onUpdate = CASCADE)
         ]
-
-// TODO add on delete strategy?
+        , indices = [Index(value = ["company_id", "document_id"])]
 )
 
 
 data class CompanyDocumentJoin(
-
         @ColumnInfo(name = "company_id") var companyId: String = "",
         @ColumnInfo(name = "document_id") var documentId: Long = 0,
         @ColumnInfo(name = "verified_at") var date: Long = 0
