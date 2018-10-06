13bb18d7c8480e5706bd85ca1c190844012e83ee
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-10-06 18:05:24 +0300

    new;

--- app/src/main/java/com/kimlic/service/entity/CompanyVerifyData.kt
+++
@@ -1,7 +0,0 @@
-package com.kimlic.service.entity
-
-import com.google.gson.annotations.SerializedName
-
-data class CompanyVerifyData(
-        @SerializedName("document") var data: CompanyVerifyData
-)
