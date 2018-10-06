4e4eeb0ad29ddfd2b1b0c1be73b2f68ad5edfc9c
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-10-06 13:58:32 +0300

    new query;

--- app/src/main/java/com/kimlic/db/dao/CompanyDocumentDao.kt
+++ app/src/main/java/com/kimlic/db/dao/CompanyDocumentDao.kt
@@ -1,5 +1,6 @@
 package com.kimlic.db.dao
 
+import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
@@ -12,7 +13,11 @@ interface CompanyDocumentDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(companyDocumentJoin: CompanyDocumentJoin)
 
-    @Query("SELECT CDJ.document_id, CDJ.company_id, CDJ.verified_at FROM company_document_join as CDJ JOIN user as U WHERE company_id=:companyId AND document_id=:documentId")
-    fun selectCompanyJoin(companyId: String, documentId: Long): CompanyDocumentJoin
+    /*
+    * Query documentDetails for single document
+    * */
+
+    @Query("SELECT CDJ.company_id, CDJ.document_id, CDJ.verified_at FROM company_document_join AS CDJ INNER JOIN company AS C ON C.id IN (SELECT COM.id FROM company as COM JOIN user as US ON US.id = COM.user_id  WHERE US.account_address =:accountAddress) JOIN document as D ON D.user_id in (SELECT US.id FROM user AS US WHERE US.account_address =:accountAddress) AND C.id=:companyId LIMIT 1")
+    fun selectCompanyDocumentJoin(accountAddress: String, companyId: String): LiveData<CompanyDocumentJoin>
 
 }
