f743d82938fd502fa4e6490800080661ffc9dbd3
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-10-12 13:59:07 +0300

    add null safety;

--- app/src/main/java/com/kimlic/service/entity/DocumentEntity.kt
+++ app/src/main/java/com/kimlic/service/entity/DocumentEntity.kt
@@ -3,9 +3,11 @@ package com.kimlic.service.entity
 import com.google.gson.annotations.SerializedName
 
 data class DocumentEntity(
-        @SerializedName("first_name") var firstName: String = "",
-        @SerializedName("last_name") var lastName: String = "",
-        @SerializedName("verified_at") var verifiedAt: String = "",
-        @SerializedName("type") var type: String = "",
-        @SerializedName("country") var countryIso: String = ""
+        @SerializedName("first_name") var firstName: String? = "",
+        @SerializedName("last_name") var lastName: String? = "",
+        @SerializedName("verified_at") var verifiedAt: String? = "1",
+        @SerializedName("status") var status: String? = "",
+        @SerializedName("reason") var reason: String? = "",
+        @SerializedName("type") var type: String? = "",
+        @SerializedName("country") var countryIso: String? = ""
 )
