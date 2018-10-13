2294d3d9b178d4b147b9598c54efbd07b709d643
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-10-13 11:25:30 +0300

    refactoring;

--- app/src/main/java/com/kimlic/service/entity/DocumentEntity.kt
+++ app/src/main/java/com/kimlic/service/entity/DocumentEntity.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class DocumentEntity(
         @SerializedName("first_name") var firstName: String? = "",
         @SerializedName("last_name") var lastName: String? = "",
-        @SerializedName("verified_at") var verifiedAt: String? = "1",
+        @SerializedName("verified_at") var verifiedAt: String? = "",
         @SerializedName("status") var status: String? = "",
         @SerializedName("reason") var reason: String? = "",
         @SerializedName("type") var type: String? = "",
