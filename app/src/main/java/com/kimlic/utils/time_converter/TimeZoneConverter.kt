ff46c1cd860f43eb507ac914687633a1a1876072
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-10-31 12:05:01 +0200

    add fun to convert milliseconds to Readable date;

--- app/src/main/java/com/kimlic/utils/time_converter/TimeZoneConverter.kt
+++ app/src/main/java/com/kimlic/utils/time_converter/TimeZoneConverter.kt
@@ -18,4 +18,10 @@ class TimeZoneConverter {
         val date = sdf.parse(timeDate)
         return date.time / 1000
     }
+
+    fun convertMillToDateString(millis: Long): String {
+        val date = Date(millis * 1000)
+        val sdf = SimpleDateFormat("dd mmm YYYY", Locale("en", "US"))
+        return sdf.format(date)
+    }
 }
