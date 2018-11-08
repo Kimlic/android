e808a4a20c986d8c1f27d92d8e3c4c9bb1c6e940
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-11-08 17:26:41 +0200

    return list of id;

--- app/src/main/java/com/kimlic/db/dao/VendorDao.kt
+++ app/src/main/java/com/kimlic/db/dao/VendorDao.kt
@@ -1,14 +1,17 @@
 package com.kimlic.db.dao
 
 import android.arch.lifecycle.LiveData
-import android.arch.persistence.room.*
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
 import com.kimlic.db.entity.VendorDocument
 
 @Dao
 interface VendorDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertDocs(docs: List<VendorDocument>)
+    fun insertDocs(docs: List<VendorDocument>): List<Long>
 
     @Query("SELECT * FROM vendor_document")
     fun selectLive(): LiveData<List<VendorDocument>>
