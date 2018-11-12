bad4ffa5e9f3c98c6ed1ddbba91aa87d3f8d0a73
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-11-12 16:11:23 +0200

    main activity espresso simple test;

--- app/src/androidTest/java/com/kimlic/MainActivityTest.kt
+++ app/src/androidTest/java/com/kimlic/MainActivityTest.kt
@@ -1,33 +1,40 @@
-//package com.kimlic
-//
-//import android.support.constraint.ConstraintLayout
-//import android.support.test.rule.ActivityTestRule
-//import org.junit.*
-//
-////@RunWith(AndroidJUnit4::class)
-//class MainActivityTest {
-//
-//    // Rule
-//
-//    @Rule @JvmField //
-//    val activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
-//
-//    private var activity: MainActivity? = null
-//
-//    @Before
-//    fun setup() {
-//        activity = activityTestRule.activity
-//    }
-//
-//    @Test
-//    fun testLaunch() {
-//        print("mainActivity test instance test")
-//        val root = activity?.findViewById<ConstraintLayout>(R.id.root)
-//        Assert.assertNotNull(root)
-//    }
-//
-//    @After
-//    fun tearDown() {
-//        activity = null
-//    }
-//}
+package com.kimlic
+
+import android.support.constraint.ConstraintLayout
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.*
+import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class MainActivityTest {
+
+    // Rule
+
+    @Rule
+    @JvmField
+    val activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    private var activity: MainActivity? = null
+
+    @Before
+    fun setup() {
+        activity = activityTestRule.activity
+    }
+
+    @Test
+    fun testLaunch() {
+        val root = activity?.findViewById<ConstraintLayout>(R.id.root)
+        Assert.assertNotNull(root)
+    }
+
+    @After
+    fun tearDown() {
+        activity = null
+    }
+}
