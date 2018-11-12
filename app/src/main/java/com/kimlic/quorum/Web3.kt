c17ed31286526ff8564e565432aacf5db02952e1
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-11-12 10:57:06 +0200

    refactoring;

--- app/src/main/java/com/kimlic/quorum/Web3.kt
+++ app/src/main/java/com/kimlic/quorum/Web3.kt
@@ -8,16 +8,12 @@ import org.web3j.protocol.Web3jFactory
 @TestOpen
 class Web3 private constructor(context: Context, URL: String) {
 
-
     val web3: Web3j = Web3jFactory.build(QuorumHttpClient(context, URL))
 
+    // Companion
+
     companion object {
-
-        // Variables
-
         private var sInstance: Web3? = null
-
-        // Public
 
         fun getInstance(URL: String, context: Context): Web3 {
             if (sInstance == null)
