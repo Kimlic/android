515b2075fcc7a4f36d23be6bd650db8d09648a22
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-11-15 12:52:04 +0200

    Enum class for settings activity;

---
+++ app/src/main/java/com/kimlic/utils/Settings.kt
@@ -0,0 +1,13 @@
+package com.kimlic.utils
+
+enum class Settings(val tag: String) {
+
+    PASSCODE("passcode"),
+    FINGERPRINT("fingerprint"),
+    DRIVE("drive"),
+    RECOVERY("recovery"),
+    TERMS("terms"),
+    PRIVACY("privacy"),
+    ABOUT("about"),
+    CHANGE("change"),
+}
