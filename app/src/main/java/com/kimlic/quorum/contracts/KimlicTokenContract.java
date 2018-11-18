de3a232d02bf18de830a27453ef816299c62e852
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-11-18 13:20:36 +0200

    renamed from KimlicToken

--- app/src/main/java/com/kimlic/quorum/contracts/KimlicTokenContract.java
+++ app/src/main/java/com/kimlic/quorum/contracts/KimlicTokenContract.java
@@ -34,7 +34,7 @@ import rx.functions.Func1;
  *
  * <p>Generated with web3j version 3.5.0.
  */
-public class KimlicToken extends Contract {
+public class KimlicTokenContract extends Contract {
     private static final String BINARY = "0x608060405234801561001057600080fd5b507f2f45a5123903bced0a1c0c407b599742400000000000000000000000000000006001819055336000908152602081905260409020556106d8806100566000396000f3006080604052600436106100985763ffffffff7c0100000000000000000000000000000000000000000000000000000000600035041663095ea7b3811461009d57806318160ddd146100d557806323b872dd146100fc578063313ce56714610126578063661884631461013b57806370a082311461015f578063a9059cbb14610180578063d73dd623146101a4578063dd62ed3e146101c8575b600080fd5b3480156100a957600080fd5b506100c1600160a060020a03600435166024356101ef565b604080519115158252519081900360200190f35b3480156100e157600080fd5b506100ea610255565b60408051918252519081900360200190f35b34801561010857600080fd5b506100c1600160a060020a036004358116906024351660443561025b565b34801561013257600080fd5b506100ea6103d2565b34801561014757600080fd5b506100c1600160a060020a03600435166024356103d7565b34801561016b57600080fd5b506100ea600160a060020a03600435166104c7565b34801561018c57600080fd5b506100c1600160a060020a03600435166024356104e2565b3480156101b057600080fd5b506100c1600160a060020a03600435166024356105c3565b3480156101d457600080fd5b506100ea600160a060020a036004358116906024351661065c565b336000818152600260209081526040808320600160a060020a038716808552908352818420869055815186815291519394909390927f8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925928290030190a350600192915050565b60015490565b6000600160a060020a038316151561027257600080fd5b600160a060020a03841660009081526020819052604090205482111561029757600080fd5b600160a060020a03841660009081526002602090815260408083203384529091529020548211156102c757600080fd5b600160a060020a0384166000908152602081905260409020546102f0908363ffffffff61068716565b600160a060020a038086166000908152602081905260408082209390935590851681522054610325908363ffffffff61069916565b600160a060020a03808516600090815260208181526040808320949094559187168152600282528281203382529091522054610367908363ffffffff61068716565b600160a060020a03808616600081815260026020908152604080832033845282529182902094909455805186815290519287169391927fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef929181900390910190a35060019392505050565b601281565b336000908152600260209081526040808320600160a060020a03861684529091528120548083111561042c57336000908152600260209081526040808320600160a060020a0388168452909152812055610461565b61043c818463ffffffff61068716565b336000908152600260209081526040808320600160a060020a03891684529091529020555b336000818152600260209081526040808320600160a060020a0389168085529083529281902054815190815290519293927f8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925929181900390910190a35060019392505050565b600160a060020a031660009081526020819052604090205490565b6000600160a060020a03831615156104f957600080fd5b3360009081526020819052604090205482111561051557600080fd5b33600090815260208190526040902054610535908363ffffffff61068716565b3360009081526020819052604080822092909255600160a060020a03851681522054610567908363ffffffff61069916565b600160a060020a038416600081815260208181526040918290209390935580518581529051919233927fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef9281900390910190a350600192915050565b336000908152600260209081526040808320600160a060020a03861684529091528120546105f7908363ffffffff61069916565b336000818152600260209081526040808320600160a060020a0389168085529083529281902085905580519485525191937f8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925929081900390910190a350600192915050565b600160a060020a03918216600090815260026020908152604080832093909416825291909152205490565b60008282111561069357fe5b50900390565b818101828110156106a657fe5b929150505600a165627a7a723058200e82e50aa35590baca1bd8a4def93b2c3a06cde6cb052b3518d7e639256120300029";
 
     public static final String FUNC_APPROVE = "approve";
@@ -80,11 +80,11 @@ public class KimlicToken extends Contract {
         _addresses.put("10", "0xd972403f899069e5872bc33c67c9be583acfa145");
     }
 
-    protected KimlicToken(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+    protected KimlicTokenContract(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
         super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
     }
 
-    protected KimlicToken(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+    protected KimlicTokenContract(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
         super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
     }
 
@@ -163,12 +163,12 @@ public class KimlicToken extends Contract {
         return executeRemoteCallSingleValueReturn(function, BigInteger.class);
     }
 
-    public static RemoteCall<KimlicToken> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
-        return deployRemoteCall(KimlicToken.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
+    public static RemoteCall<KimlicTokenContract> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(KimlicTokenContract.class, web3j, credentials, gasPrice, gasLimit, BINARY, "");
     }
 
-    public static RemoteCall<KimlicToken> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
-        return deployRemoteCall(KimlicToken.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
+    public static RemoteCall<KimlicTokenContract> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return deployRemoteCall(KimlicTokenContract.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, "");
     }
 
     public List<ApprovalEventResponse> getApprovalEvents(TransactionReceipt transactionReceipt) {
@@ -241,12 +241,12 @@ public class KimlicToken extends Contract {
         return transferEventObservable(filter);
     }
 
-    public static KimlicToken load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
-        return new KimlicToken(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    public static KimlicTokenContract load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return new KimlicTokenContract(contractAddress, web3j, credentials, gasPrice, gasLimit);
     }
 
-    public static KimlicToken load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
-        return new KimlicToken(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    public static KimlicTokenContract load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new KimlicTokenContract(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
     }
 
     protected String getStaticDeployedAddress(String networkId) {
