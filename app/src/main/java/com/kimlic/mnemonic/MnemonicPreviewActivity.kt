edd187dbab6bd02da87323043db38569a26cc925
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-11-19 17:16:56 +0200

    use MnemonicPreviewViewModel; use ClipBoardUtil;

--- app/src/main/java/com/kimlic/mnemonic/MnemonicPreviewActivity.kt
+++ app/src/main/java/com/kimlic/mnemonic/MnemonicPreviewActivity.kt
@@ -1,21 +1,18 @@
 package com.kimlic.mnemonic
 
 import android.arch.lifecycle.ViewModelProviders
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
 import android.os.Bundle
 import com.kimlic.BaseActivity
 import com.kimlic.R
 import com.kimlic.managers.PresentationManager
-import com.kimlic.model.ProfileViewModel
+import com.kimlic.utils.ClipBoardUtil
 import kotlinx.android.synthetic.main.activity_mnemonic_preview.*
 
 class MnemonicPreviewActivity : BaseActivity() {
 
     // Variables
 
-    private lateinit var model: ProfileViewModel
+    private lateinit var model: MnemonicPreviewViewModel
 
     // Life
 
@@ -23,7 +20,7 @@ class MnemonicPreviewActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mnemonic_preview)
 
-        model = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        model = ViewModelProviders.of(this).get(MnemonicPreviewViewModel::class.java)
         setupUI()
     }
 
@@ -40,7 +37,7 @@ class MnemonicPreviewActivity : BaseActivity() {
                     showPopupImmersive(message = getString(R.string.write_down_these_words_store_them_in_a_safe_place))
                 }
                 "copy" -> {
-                    copyToBuffer(mnemonicList())
+                    ClipBoardUtil.copyToClipBoard(this, mnemonicList())
                     phraseBt.tag = "save"
                     phraseBt.text = getString(R.string.ok_i_save_the_passphrase)
                 }
@@ -56,16 +53,5 @@ class MnemonicPreviewActivity : BaseActivity() {
         listView.adapter = PhraseAdapter(this, R.layout.item_phrase, list)
     }
 
-    private fun mnemonicList() = model.user().mnemonic.split(" ")
-
-    private fun copyToBuffer(listToSave: List<String>) {
-        val clipBoard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val string = StringBuilder()
-
-        if (listToSave.isNotEmpty()) {
-            listToSave.forEachIndexed { _, it -> string.append("$it ") }
-            val clipData = ClipData.newPlainText("copied text", string)
-            clipBoard.primaryClip = clipData
-        }
-    }
+    private fun mnemonicList() = model.userMnemonic().split(" ")
 }
