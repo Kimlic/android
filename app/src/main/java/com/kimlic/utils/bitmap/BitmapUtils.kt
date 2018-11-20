9b22e145bc2b662428a74be4371557d750c112ef
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-11-20 18:51:21 +0200

    remove unused function;

--- app/src/main/java/com/kimlic/utils/bitmap/BitmapUtils.kt
+++ app/src/main/java/com/kimlic/utils/bitmap/BitmapUtils.kt
@@ -5,21 +5,6 @@ import android.graphics.Matrix
 
 object BitmapUtils {
 
-    fun getResizedBitmap(bm: Bitmap, newWidth: Int, newHeight: Int, angel: Float, isNecessaryToKeepOrig: Boolean): Bitmap {
-        val width = bm.width
-        val height = bm.height
-        val scaleWidth = newWidth.toFloat() / width
-        val scaleHeight = newHeight.toFloat() / height
-        val matrix = Matrix()
-        matrix.postScale(scaleWidth, scaleHeight)
-        matrix.postRotate(angel)
-        val resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false)
-
-        if (!isNecessaryToKeepOrig) bm.recycle()
-
-        return resizedBitmap
-    }
-
     fun getScaledBitmap(bitmap: Bitmap, newWidth: Int, newHeight: Int, filter: Boolean) = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, filter)
 
     fun rotateBitmap(source: Bitmap, angel: Float): Bitmap {
