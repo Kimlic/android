721dc5d14fcf7a67114cffe53c07fe470ca94e35
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-11-21 10:05:58 +0200

    add new function witch allows to transform by uri;

--- app/src/main/java/com/kimlic/utils/mappers/FileNameTxtBase64ToBitmap.kt
+++ app/src/main/java/com/kimlic/utils/mappers/FileNameTxtBase64ToBitmap.kt
@@ -2,25 +2,34 @@ package com.kimlic.utils.mappers
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Base64
 import android.util.Log
 import com.kimlic.KimlicApp
 import java.io.File
 import java.io.IOException
 
+/*
+* This util class gets file name from app folder directory
+ * */
+
 class FileNameTxtBase64ToBitmap : BaseMapper<String, Bitmap?> {
 
     // life
 
     override fun transform(input: String): Bitmap? {
-        try {
-            val fileByteArrayBase64 = File(KimlicApp.applicationContext().filesDir.toString() + "/" + input).readBytes()
+        val fileByteArrayBase64Uri = Uri.parse(KimlicApp.applicationContext().filesDir.toString() + "/" + input)
+        return transformByUri(fileByteArrayBase64Uri)
+    }
+
+    fun transformByUri(uri: Uri): Bitmap? {
+        return try {
+            val fileByteArrayBase64 = File(uri.path).readBytes()
             val decodedString = Base64.decode(fileByteArrayBase64, Base64.DEFAULT)
-            val bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
-            return bitmap
+            BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
         } catch (e: IOException) {
             Log.d("MAPPER", "io exception!!!")
-            return null
+            null
         }
     }
 }
